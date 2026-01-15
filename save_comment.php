6d224c666aa9f0674f541221d9a10444356f3a0f
rrei25872-oss <rrei25872@gmail.com>
2026-01-15 14:29:07 +0800

    Update save_comment.php

--- save_comment.php
+++ save_comment.php
@@ -4,7 +4,9 @@ $data = json_decode(file_get_contents($file), true);
 
 $data[] = [
     "name" => htmlspecialchars($_POST["name"]),
-    "comment" => htmlspecialchars($_POST["comment"])
+    "text" => htmlspecialchars($_POST["comment"]),
+    "time" => date("Y-m-d H:i"),
+    "replies" => []
 ];
 
 file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));

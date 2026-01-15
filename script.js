c318c5712f36371faa073d7e281592666d14d430
rrei25872-oss <rrei25872@gmail.com>
2026-01-15 14:19:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,36 @@ document.addEventListener("DOMContentLoaded", () => {
             document.querySelector(link.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
         });
     });
+
+    const form = document.getElementById("commentForm");
+    const list = document.getElementById("commentList");
+
+    function loadComments() {
+        fetch("comments.json")
+            .then(res => res.json())
+            .then(data => {
+                list.innerHTML = "";
+                data.reverse().forEach(c => {
+                    list.innerHTML += `
+                        <div class="comment">
+                            <strong>${c.name}</strong>
+                            <p>${c.comment}</p>
+                        </div>
+                    `;
+                });
+            });
+    }
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        fetch("save_comment.php", {
+            method: "POST",
+            body: new FormData(form)
+        }).then(() => {
+            form.reset();
+            loadComments();
+        });
+    });
+
+    loadComments();
 });
